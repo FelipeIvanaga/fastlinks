89ad925466263bb1f0e7d841a138752357923e52
Felipe Ivanaga <felipeivanaga@pm.me>
2022-09-14 16:08:57 -0300

    fix: footer position on big screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
 	return (
-		<div className="absolute bg-indigo-600 p-4 text-white bottom-0 w-screen flex flex-row justify-center gap-4">
+		<div className="absolute bg-indigo-600 p-4 text-white bottom-0 left-0 w-screen flex flex-row justify-center gap-4">
 			<a href="https://github.com/FelipeIvanaga/fastlinks">Project Repo</a>
 			-
 			<a href="https://github.com/FelipeIvanaga">Created by Felipe Ivanaga</a>
