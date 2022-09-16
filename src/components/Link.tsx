f0e0cfa733faf1c91462ff4cc4855edcb6faaf88
Felipe Ivanaga <felipeivanaga@pm.me>
2022-09-16 12:24:35 -0300

    fix: new links reference

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -7,7 +7,7 @@ type LinkProps = {
 export function Link({ link }: LinkProps) {
 	return (
 		<div>
-			<a id="test" href={link.address} target="blank">
+			<a id="test" href={link.address} target="_blank" rel="noreferrer noopener">
 				<button className="bg-indigo-500 bg-opacity-80 px-4 py-2 rounded-full shadow hover:bg-opacity-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition-all">
 					{link.name}
 				</button>
